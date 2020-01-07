2cb778b77c14a99d17d7b84f44dd4ead1324669c
Ben Taylor <ben10taylor10@hotmail.com>
2020-01-07 12:18:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,10 +20,10 @@ def monopoly_with_third_tier
   {
     :railroads => {:pieces => 4, 
     :rent_in_dollars => {
-        :key1 => {},
-        :key2 => {},
-        :key3 => {},
-        :key4 => {}
+        :price1 => {},
+        :price2 => {},
+        :price3 => {},
+        :price4 => {}
       },
       :names => {
         :name1 => 'barry',
